9d6c5a11497749093f71ca8ba83c52bdb8db490e
Jukoleda <mde_96@hotmail.com>
2021-07-29 23:02:17 -0300

    primer commit, se agrega componente, modelo, servicio de producto

---
+++ cliente/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListarProductosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
